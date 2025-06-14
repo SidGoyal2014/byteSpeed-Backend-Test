1bad94873559fbea9fced429773e4cd99bbb6ab2
Siddharth Goyal <sidgoyal2014@gmail.com>
2025-06-14 11:50:29 +0530

    Controller init

---
+++ src/main/java/bitespeed/backend/test/controller/ContactController.java
@@ -0,0 +1,15 @@
+package bitespeed.backend.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/contacts")
+public class ContactController {
+
+    @GetMapping("/")
+    public String addContact(){
+        return "hello";
+    }
+}
