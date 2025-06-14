960a179757a26816db4ed1862afb2e3d83908af5
Siddharth Goyal <sidgoyal2014@gmail.com>
2025-06-15 01:01:21 +0530

    Gift Controller Code Refactor

--- src/main/java/bitespeed/backend/test/controller/ContactController.java
+++ src/main/java/bitespeed/backend/test/controller/ContactController.java
@@ -1,18 +1,28 @@
 package bitespeed.backend.test.controller;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import bitespeed.backend.test.services.ContactService;
+import bitespeed.backend.test.dto.ContactDTO.IdentifyContactRequestDTO;
 
 @RestController
 @RequestMapping("/")
 public class ContactController {
 
-    @PostMapping("/identify")
-    public ResponseEntity<?> indetifyContact() {
+    private ContactService contactService;
 
-        return ResponseEntity.ok("Hello");
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @PostMapping("/identify")
+    public ResponseEntity<?> indetifyContact(@RequestBody IdentifyContactRequestDTO request) {
+        return ResponseEntity.ok().body(contactService.identifyContact(request));
     }
 }
